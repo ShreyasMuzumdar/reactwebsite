a278f4afcc2679c658a6fdb0c5485489911deb1f
ShreyasMuzumdar <shreyas2966@gmail.com>
2025-11-25 00:28:59 +0100

    plug

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,13 @@ import Projects from './components/Projects'
 import Robots from './components/Robots'
 import Resume from './components/Resume'
 import Contact from './components/Contact'
-import GeometricPatterns from './components/GeometricPatterns'
+import CircuitBackground from './components/CircuitBackground'
 
 function App() {
   return (
     <ThemeProvider>
       <div className="App">
-        <GeometricPatterns 
-          density="medium" 
-          speed="slow" 
-          opacity={0.15}
-          colors={['#FFE135', '#FFD700', '#F4D03F', '#FFF59D', '#FFF9C4']}
-          className="fixed-background"
-        />
+        <CircuitBackground />
         <Header />
         <main>
           <AboutMe />
